53c4bfc0a027f9b93e652deef63ee016ccf3aeff
Sam Albon <sam.albon@enthusiastgaming.com>
2019-06-19 19:08:52 -0400

    Changed dockerfile to not append pipe.sh with working directory because Dockerhub doesn't like it (?)

---
+++ pipe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+curl \
+    -X POST "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE/purge_cache" \
+    -H "X-Auth-Email:$CLOUDFLARE_AUTH_EMAIL" \
+    -H "X-Auth-Key:$CLOUDFLARE_AUTH_KEY" \
+    -H "Content-Type:application/json" \
+    --data '{"files":["$URL_CACHE_PATH"]}'
