6ce794f4181d28bfde082c6ff6f2adc680fc079b
Sam Albon <sam.m.albon@outlook.com>
2019-08-30 11:55:45 -0400

    Changed script to take an array of strings

--- pipe.sh
+++ pipe.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-curl \
-    -X POST "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE/purge_cache" \
-    -H "X-Auth-Email:$CLOUDFLARE_AUTH_EMAIL" \
-    -H "X-Auth-Key:$CLOUDFLARE_AUTH_KEY" \
-    -H "Content-Type:application/json" \
-    --data '{"files":["${URL_CACHE_PATH}"]}'
+for file in "${URL_CACHE_PATHS[@}}"; do
+    curl \
+        -X POST "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE/purge_cache" \
+        -H "X-Auth-Email:$CLOUDFLARE_AUTH_EMAIL" \
+        -H "X-Auth-Key:$CLOUDFLARE_AUTH_KEY" \
+        -H "Content-Type:application/json" \
+        --data $(jq -n --arg files "$file" '{ files: [$files] }')
+done
